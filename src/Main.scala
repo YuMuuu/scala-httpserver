b71e43072d24e902e58c4f82d46af45c6c74e167
YuMuuu <c01150788f@edu.teu.ac.jp>
2018-02-07 20:19:26 +0900

    メッセージボディを取得

--- src/Main.scala
+++ src/Main.scala
@@ -1,8 +1,40 @@
+import java.io._
+import java.net._
+
+import scala.collection.JavaConverters._
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    println("start >>>")
+
+    val server = new ServerSocket(80)
+    val socket = server.accept()
+    val in = new BufferedReader(new InputStreamReader(socket.getInputStream, "UTF-8"))
+
+    val header = new StringBuilder()
+    val body = new StringBuilder()
+
+    var contentLength = 0
+
+    //@throws (classOf[IOException])
+    Iterator.continually(in.readLine()).takeWhile(it => it !=null && !it.isEmpty).foreach {
+      line =>
+        header.append(line + "\n");
+        if(line.startsWith("Content-Length")){
+          contentLength = line.split(":")(1).trim.toInt
+          //println(contentLength)
+        }
+    }
+
+    if (0 < contentLength) {
+      var c = new Array[Char](contentLength)
+      in.read(c)
+      body.append(String.valueOf(c))
+    }
 
 
-class Main {
-  
-  def main(args: Array[String]){
-    println("start >>> for scala")
+
+    println(header)
+    println(body)
+    println("<<< end")
   }
-}
