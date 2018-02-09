95be2a6032ad84967c5b0a7621337ef0ce4f48bb
YuMuuu <c01150788f@edu.teu.ac.jp>
2018-02-09 14:48:02 +0900

    リクエスト部分の別オブジェクトへ書き直し

--- src/main/scala/HttpRequset.scala
+++ src/main/scala/HttpRequset.scala
@@ -1,14 +1,26 @@
+import java.io.{BufferedReader, InputStream, InputStreamReader}
 //import java.io_
 //import java.net_
-//
+
 object HttpRequset {
-//
-//
-  class HttpRequest{
-//    val CRLF = "\r\n"
-//
-//    class
-//
-  }
-//
+
+ class HttpRequest{
+   val CRLF = "\r\n"
+   var hedderText = ""
+   var bodyText = ""
+
+   def HttpReqest(input: InputStream):Unit = {
+     val in = new BufferedReader(new InputStreamReader(input, "UTF-8"))
+   }
+   def readHedder(in: BufferReader):Unit ={
+     val line = in.readLine()
+     val header = new StringBuilder()
+     var contentLength = 0
+
+     Iterator.continually(in.readLine()).takeWhile(it => it != null && !it.isEmpty).foreach {
+       line =>
+         header.append(line + "\n")
+     }
+   }
+ }
 }
