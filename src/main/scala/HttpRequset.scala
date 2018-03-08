cb691386660a104b78a3569df291dff6cdebc13c
YuMuuu <c01150788f@edu.teu.ac.jp>
2018-03-09 02:28:54 +0900

    recommit

--- src/main/scala/HttpRequset.scala
+++ src/main/scala/HttpRequset.scala
@@ -1,30 +1,65 @@
 import java.io.{BufferedReader, InputStream, InputStreamReader}
-//import java.io_
-//import java.net_
+
+import scala.collection.immutable.Stream
 
 object HttpRequset {
 
- class HttpRequest{
-   val CRLF = "\r\n"
-   val hedderText = ""
-   val bodyText = ""
+  class HttpRequest {
+    val CRLF = "\r\n"
+    val headerText = new StringBuilder()
+    val bodyText = new StringBuilder()
 
-   def HttpReqest(input: InputStream):Unit = {
-     val in = new BufferedReader(new InputStreamReade r(input, "UTF-8"))
-   }
+    def HttpRequest(input: InputStream): Unit = {
+      val in = new BufferedReader(new InputStreamReader(input, "UTF-8"))
+      headerText.append(readHeader(in))
+      bodyText.append(readBody(in))
 
-   def readHedder(in: BufferedReader):Unit ={
-     val line = in.readLine()
-     val header = new StringBuilder()
-     var contentLength = 0
+    }
 
-     Iterator.continually(in.readLine()).takeWhile(it => it != null && !it.isEmpty).foreach {
-       line =>
-         header.append(line + "\n")
-     }
-     header.toString();
-   }
+    def readHeader(in: BufferedReader): Option[String] = {
+      val line = in.readLine()
+      val header = new StringBuilder()
+
+      if (line == null) {
+        None
+      } else {
+        Iterator.continually(in.readLine()).takeWhile(it => it != null && !it.isEmpty).foreach {
+          line => header.append(line + "\n")
+        }
+        Some(header.toString())
+      }
+    }
+
+    def readBody(in: BufferedReader): Option[String] = {
+      val contentLength = getContentLength()
+      if (contentLength <= 0) {
+        None
+      } else {
+        val c = new Array[Char](contentLength)
+        in.read(c)
+        Some(String.valueOf(c))
+      }
+    }
+
+    def getContentLength(): Integer = {
+//      Stream.of(headerText.split(CRLF))
+//        .filter(_.startsWith("Content-Length"))
+//        .map(_.split(":")(1).trim)
+//        .m apToInt (Integer.parseInt)
+//        .findFirst.orElse(0)
 
 
- }
+    }
+
+    def getHeaderText(): String = {
+      headerText.toString()
+    }
+
+    def getBodyText(): String = {
+      bodyText.toString()
+    }
+
+
+  }
+
 }
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,16 +1,8 @@
 import java.io._
 import java.net._
-
-import scala.collection.JavaConverters._
+import HttpRequset.HttpRequest
 
 object Main {
-
-
-  def safeStringToInt(str: String): Option[Int] = {
-    import scala.util.control.Exception._
-    catching(classOf[NumberFormatException]) opt str.toInt
-  }
-
 
   def main(args: Array[String]): Unit = {
     println("start >>>")
@@ -18,30 +10,8 @@ object Main {
     val server = new ServerSocket(80)
     val socket = server.accept()
     val in = new BufferedReader(new InputStreamReader(socket.getInputStream, "UTF-8"))
+    val request = new HttpRequest(in)
 
-    val header = new StringBuilder()
-    val body = new StringBuilder()
-
-    var contentLength = 0
-
-    Iterator.continually(in.readLine()).takeWhile(it => it != null && !it.isEmpty).foreach {
-      line =>
-        header.append(line + "\n");
-        if (line.startsWith("Content-Length")) {
-          contentLength = line.split(":")(1).trim.toInt
-        }
-    }
-
-    if (0 < contentLength) {
-      val c = new Array[Char](contentLength)
-      in.read(c)
-      body.append(String.valueOf(c))
-    }
-
-
-    println(header)
-    println(body)
-    println()
     println("<<< end")
   }
 }
